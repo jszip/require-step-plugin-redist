c02f28680e82d93c0f263f6992326521f48681e7
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-02-22 12:05:05 +0000

    Add update-check

---
+++ update-check.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'nokogiri'
+require 'json'
+require 'open-uri'
+
+current = Gem::Version.new(JSON.parse(open('https://github.com/requirejs/step/raw/master/package.json').read)["version"])
+
+maven = Gem::Version.new(Nokogiri::XML(File.open(File.expand_path('../pom.xml', __FILE__))).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+
+puts "["+File.dirname(__FILE__)+"]   Next redist: " + maven.to_s
+puts "["+File.dirname(__FILE__)+"] Last upstream: " + current.to_s
+
+if current >= maven
+	puts "["+File.dirname(__FILE__)+"] A release can be made!!!"
+	exit 1 
+end
+puts "["+File.dirname(__FILE__)+"] Everything up to date."
